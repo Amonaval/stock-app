60ad520c9f169ec38eb919266f492d44f9360048
Amol Naval <amolnava@amdocs.com>
2019-03-13 00:08:34 +0530

    Updates

---
+++ precache-manifest.ed5e2a1d870e4d66519e592c71f3d4de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "784508198b6891172213",
+    "url": "/stock-app/static/js/runtime~main.64d052a9.js"
+  },
+  {
+    "revision": "e8224695158735c6ffb7",
+    "url": "/stock-app/static/js/main.4b14e17c.chunk.js"
+  },
+  {
+    "revision": "7931b37f0215f3976af9",
+    "url": "/stock-app/static/js/2.a1412644.chunk.js"
+  },
+  {
+    "revision": "e8224695158735c6ffb7",
+    "url": "/stock-app/static/css/main.ec6d13bb.chunk.css"
+  },
+  {
+    "revision": "76adab2d4b52d8e34406f2ea26fa9a9b",
+    "url": "/stock-app/index.html"
+  }
+];
